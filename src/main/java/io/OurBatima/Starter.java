3de31fd7a03ef25e124e0e1ef51224daef00b323
wael-ben-salem <108833476+wael-ben-salem@users.noreply.github.com>
2025-02-13 11:27:25 +0100

    Update Starter.java

--- src/main/java/io/OurBatima/Starter.java
+++ src/main/java/io/OurBatima/Starter.java
@@ -4,10 +4,7 @@ package io.OurBatima;
 
 import io.OurBatima.core.Main;
 
-/**
- * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
- * Create on  19/04/2023
- */
+
 public class Starter extends Main {
 
 
